31c2f1237b0ac81aca5ab6685c5ca38c5c85e026
Edgar Alagyozyan Ubuntu <alagyozyan@gmail.com>
2023-02-07 16:35:44 +0400

    Updated two scripts

--- 02_relation.py
+++ 02_relation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-num1 = int(input("Enter the first numner: "))
-num2 = int(input("Enter the second numner: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 if num1 > num2:
 	print(num1, "is greater than", num2)
