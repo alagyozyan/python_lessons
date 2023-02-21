e61446d0c2126672ca90591aba920fdf42b2a8b5
Edgar Alagyozyan Ubuntu <alagyozyan@gmail.com>
2023-02-21 15:17:55 +0400

    Renamed some scripts

--- classworskAndHomeworks/functions/08_reverse_string.py
+++ classworskAndHomeworks/functions/08_reverse_string.py
@@ -3,7 +3,10 @@
 def str_reverse (s):
 	return s[::-1]
 
-s = input ("Please enter some string : ")
-reversed_result = str_reverse (s)
-print ("Here is the reversed string you entered : ", reversed_result)
+def main ():
+	s = input ("Please enter some string : ")
+	reversed_result = str_reverse (s)
+	print ("Here is the reversed string you entered : ", reversed_result)
+
+main()
 
